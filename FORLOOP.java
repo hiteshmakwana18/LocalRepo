6c381284f936ccbfd296a1d3e8ce768149cf6050
Hitesh <hitesh.ddu@gmail.com>
2024-08-06 10:07:26 +0530

    my first commit

---
+++ FORLOOP.java
@@ -0,0 +1,8 @@
+public class FORLOOP {
+    public static void main(String[] args) {
+        int[] a={9,2,20,100,5};
+        for(int i :a){
+            System.out.println(i);
+        }
+    }
+}
